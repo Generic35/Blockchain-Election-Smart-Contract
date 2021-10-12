48dedf7c2754089750f610ccd4bd8bf7c357785d
Eric Lamontagne <ricklamontagne@gmail.com>
2021-10-12 10:18:56 -0400

    feat: Add the ability to store candidates in the contract, including unit tests

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,26 @@
 pragma solidity ^0.5.16;
 
 contract Election {
+	// model a candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+	// store the candate
+	// fetch the candidate from storeage
+	mapping(uint => Candidate) public candidates;
 
-	string public candidate;
+	// store candidate count
+	uint public candidatesCount;
+
 	constructor () public {
-		candidate = "Candidate 1";
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate(string memory _name) private {
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
 }
