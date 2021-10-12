c174b1791643297dcb40329d7fad60ab397ed861
Eric Lamontagne <ricklamontagne@gmail.com>
2021-10-12 14:02:42 -0400

    feat: Add ability to vote and requirements that the voter has not voted already

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.5.16;
 
 contract Election {
+	    // Store accounts that have voted
+    mapping(address => bool) public voters;
 	// model a candidate
 	struct Candidate {
 		uint id;
@@ -14,6 +16,10 @@ contract Election {
 	// store candidate count
 	uint public candidatesCount;
 
+ // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
 	constructor () public {
 		addCandidate("Candidate 1");
 		addCandidate("Candidate 2");
@@ -23,4 +29,21 @@ contract Election {
 		candidatesCount++;
 		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
+
+	function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
+    }
 }
